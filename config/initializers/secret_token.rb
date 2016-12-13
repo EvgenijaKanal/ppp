f83dca4c8e9b7cdc1006f4304770858f0cba364c
EvgenijaKanal <evgenija.kanal@inbox.lv>
2016-12-13 10:04:07 +0200

    Init repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eva::Application.config.secret_key_base = '2f308760afd9ba37d36ea384bfd2913122b13437148f8d06f7f259375196fc8431498a23b327a25e19eb6ccfc0b4c8622c75d9725fa43a85405c09af4411a123'
